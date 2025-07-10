1cbc40b60da4b691f9c701e110d8100058a7e37e
pranalikavade <pranalikavade73@gmail.com>
2025-07-10 16:26:38 +0530

    Update Product.java

--- src/main/java/com/bakery/model/Product.java
+++ src/main/java/com/bakery/model/Product.java
@@ -2,4 +2,12 @@ package com.bakery.model;
 
 public class Product {
 
+	public double price;
+	public String name;
+	public int id;
+	public int stock;
+public String toString()
+{
+	return id+","+name+"-"+price+"(Stock:"+stock+")";
+}
 }
