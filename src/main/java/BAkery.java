e0eebd47947612d6f47a81590c0dd4fdf1b3248e
pranalikavade <pranalikavade73@gmail.com>
2025-07-10 16:31:24 +0530

    Update BAkery.java

--- src/main/java/BAkery.java
+++ src/main/java/BAkery.java
@@ -5,6 +5,6 @@ import com.bakery.service.BakeryService;
 public class Bakery {
     public static void main(String[] args) {
         BakeryService service = new BakeryService();
-        service.run(); // Start the console application
+        service.run(); 
     }
 }
