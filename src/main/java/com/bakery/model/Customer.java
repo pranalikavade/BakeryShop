f79aa068d36b0c61a00e476959ce25d2f13949a3
pranalikavade <pranalikavade73@gmail.com>
2025-07-10 16:24:31 +0530

    Update Customer.java

--- src/main/java/com/bakery/model/Customer.java
+++ src/main/java/com/bakery/model/Customer.java
@@ -1,5 +1,13 @@
-package com.bakery.model;
+ package com.bakery.model;
 
 public class Customer {
+    public int id;
+    public String name;
+    public String phone;
 
+    @Override
+    public String toString() {
+        return id + ". " + name + " (" + phone + ")";
+    }
 }
+
